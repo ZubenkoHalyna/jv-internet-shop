c4a7fa107a236e0414a0bed1bb46c1db87371ff8
Galina <gala6ap@mail.ru>
2020-04-29 16:43:16 +0300

    refactoring: controller renamed

--- src/main/java/mate/academy/internetshop/controller/DeleteProductFromShoppingCartController.java
+++ src/main/java/mate/academy/internetshop/controller/DeleteProductFromShoppingCartController.java
@@ -9,7 +9,7 @@ import mate.academy.internetshop.lib.Injector;
 import mate.academy.internetshop.service.ProductService;
 import mate.academy.internetshop.service.ShoppingCartService;
 
-public class DeleteProductFromShoppingCart extends HttpServlet {
+public class DeleteProductFromShoppingCartController extends HttpServlet {
     private static final long USER_ID = 0;
     private static final Injector INJECTOR =
             Injector.getInstance("mate.academy.internetshop");
