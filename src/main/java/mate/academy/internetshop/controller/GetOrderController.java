23856d37854784e8fb8a1369eebc7ddcaaf97b02
Galina <gala6ap@mail.ru>
2020-04-29 15:27:32 +0300

    refactoring: controllers renamed

--- src/main/java/mate/academy/internetshop/controller/GetOrderController.java
+++ src/main/java/mate/academy/internetshop/controller/GetOrderController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import mate.academy.internetshop.lib.Injector;
 import mate.academy.internetshop.service.OrderService;
 
-public class OrderController extends HttpServlet {
+public class GetOrderController extends HttpServlet {
     private static final Injector INJECTOR =
             Injector.getInstance("mate.academy.internetshop");
     private OrderService orderService =
--- src/main/java/mate/academy/internetshop/controller/GetShoppingCartController.java
+++ src/main/java/mate/academy/internetshop/controller/GetShoppingCartController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import mate.academy.internetshop.lib.Injector;
 import mate.academy.internetshop.service.ShoppingCartService;
 
-public class ShoppingCartController extends HttpServlet {
+public class GetShoppingCartController extends HttpServlet {
     private static final long USER_ID = 0;
     private static final Injector INJECTOR =
             Injector.getInstance("mate.academy.internetshop");
