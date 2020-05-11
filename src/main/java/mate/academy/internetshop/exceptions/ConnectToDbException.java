ad391527d76ec2bd066b6aab55fc11bfddc25fc5
ZubenkoHalyna <gala6ap@mail.ru>
2020-05-11 10:43:43 +0300

    hw10
    JDBC implementation of ProductDao added

---
+++ src/main/java/mate/academy/internetshop/exceptions/ConnectToDbException.java
@@ -0,0 +1,7 @@
+package mate.academy.internetshop.exceptions;
+
+public class ConnectToDbException extends RuntimeException {
+    public ConnectToDbException(Throwable cause) {
+        super(cause);
+    }
+}
